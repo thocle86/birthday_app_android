1cdc79e4374a450799bda9d01d8c9c68a2907a35
Thomas Clement <thocle86@gmail.com>
2022-08-25 11:38:45 +0200

    Ajout de l'activité Splash pour le démarrage de l'appli

---
+++ app/src/main/java/fr/cefim/android/birthday_app/activities/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package fr.cefim.android.birthday_app.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        if (login ok){
+//            startActivity(new Intent(this, MainActivity.class));
+//        } else{
+//            startActivity(new Intent(this, LoginActivity.class));
+//        }
+        finish();
+
+    }
+}
